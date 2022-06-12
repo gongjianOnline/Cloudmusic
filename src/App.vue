90e414fe83e4aeccb8425a49b3f9bf25903fd68a
龚箭 <123456@163.com>
2022-06-12 19:28:22 +0800

    优化滚动条

--- src/App.vue
+++ src/App.vue
@@ -3,16 +3,17 @@
 </template>
 
 <script setup>
-
 </script>
 
 <style>
-*{
+* {
   box-sizing: border-box;
   margin: 0;
   padding: 0;
 }
-html,body,#app{
+html,
+body,
+#app {
   width: 100%;
   height: 100%;
 }
@@ -29,5 +30,24 @@ html,body,#app{
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+}
+/* 滚动条优化 */
+/* 设置滚动条的样式 */
+::-webkit-scrollbar {
+  width: 6px;
+}
+/* 滚动槽 */
+::-webkit-scrollbar-track {
+  -webkit-box-shadow: inset006pxrgba(0, 0, 0, 0.3);
+  border-radius: 10px;
+}
+/* 滚动条滑块 */
+::-webkit-scrollbar-thumb {
+  border-radius: 10px;
+  background: rgba(0, 0, 0, 0.1);
+  -webkit-box-shadow: inset006pxrgba(0, 0, 0, 0.5);
+}
+::-webkit-scrollbar-thumb:window-inactive {
+  background: rgba(0, 0, 0, 0.1);
 }
 </style>
